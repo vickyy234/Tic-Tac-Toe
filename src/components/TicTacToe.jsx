d5cffd5e10c6087bd621b72a500faaf673a6493a
vickyy234 <vickymayilsamy@gmail.com>
2025-03-26 22:56:10 +0530

    Updated

--- src/components/TicTacToe.jsx
+++ src/components/TicTacToe.jsx
@@ -2,7 +2,23 @@ import '../styles/TicTacToe.css'
 
 function TicTacToe() {
     return (
-        <></>
+        <div className='container'>
+            <div className='container-two'>
+                <button></button>
+                <button></button>
+                <button></button>
+            </div>
+            <div className='container-two'>
+                <button></button>
+                <button></button>
+                <button></button>
+            </div>
+            <div className='container-two'>
+                <button></button>
+                <button></button>
+                <button></button>
+            </div>
+        </div>
     )
 }
 
